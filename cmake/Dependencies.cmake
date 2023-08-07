f02ffa7be57b5e0a0605b68cda1f7716266c4520
LigasN <norbert221198@gmail.com>
2023-08-07 23:09:48 +0200

    13 - one cmakelists, some win linking successful

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -12,47 +12,47 @@ function(setup_dependencies)
     cpmaddpackage("gh:catchorg/Catch2@3.3.2")
   endif()
 
-  if(NOT TARGET gettext)
-    cpmaddpackage(
-      NAME
-      gettext
-      VERSION
-      0.21-v1.16
-      URL
-      https://github.com/mlocati/gettext-iconv-windows/releases/download/v0.21-v1.16/gettext0.21-iconv1.16-shared-64.zip
-      DOWNLOAD_ONLY
-      YES)
-    if(gettext_ADDED)
-      # lua has no CMake support, so we create our own target
+  # if(NOT TARGET gettext)
+  #   cpmaddpackage(
+  #     NAME
+  #     gettext
+  #     VERSION
+  #     0.21-v1.16
+  #     URL
+  #     https://github.com/mlocati/gettext-iconv-windows/releases/download/v0.21-v1.16/gettext0.21-iconv1.16-shared-64.zip
+  #     DOWNLOAD_ONLY
+  #     YES)
+  #   if(gettext_ADDED)
+  #     # lua has no CMake support, so we create our own target
 
-      # file(GLOB gettext_sources ${gettext_SOURCE_DIR}/*.c)
-      # list(
-      #   REMOVE_ITEM
-      #   lua_sources
-      #   "${lua_SOURCE_DIR}/lua.c"
-      #   "${lua_SOURCE_DIR}/luac.c")
-      # add_library(lua STATIC ${lua_sources})
+  #     # file(GLOB gettext_sources ${gettext_SOURCE_DIR}/*.c)
+  #     # list(
+  #     #   REMOVE_ITEM
+  #     #   lua_sources
+  #     #   "${lua_SOURCE_DIR}/lua.c"
+  #     #   "${lua_SOURCE_DIR}/luac.c")
+  #     # add_library(lua STATIC ${lua_sources})
 
-      add_library(
-        gettext
-        SHARED
-        IMPORTED
-        GLOBAL)
-      add_library(
-        intl
-        SHARED
-        IMPORTED
-        GLOBAL)
+  #     add_library(
+  #       gettext
+  #       SHARED
+  #       IMPORTED
+  #       GLOBAL)
+  #     add_library(
+  #       intl
+  #       SHARED
+  #       IMPORTED
+  #       GLOBAL)
 
-      target_include_directories(gettext PUBLIC $<BUILD_INTERFACE:${gettext_SOURCE_DIR}>)
-      target_include_directories(intl PUBLIC $<BUILD_INTERFACE:${gettext_SOURCE_DIR}>)
+  #     target_include_directories(gettext PUBLIC $<BUILD_INTERFACE:${gettext_SOURCE_DIR}>)
+  #     target_include_directories(intl PUBLIC $<BUILD_INTERFACE:${gettext_SOURCE_DIR}>)
 
-      # set_target_properties(
-      #   gettext PROPERTIES IMPORTED_LOCATION ${CMAKE_SOURCE_DIR}/external/gettext/libgettextlib-0-21.dll
-      # )#.lib meybe or ${GETTEXT_LIBRARY}
-      # target_link_libraries(${CMAKE_PROJECT_NAME} PRIVATE ${DEPENDENCIES} gettext) # ${GETTEXT_LIBRARIES}
-      # target_include_directories(${CMAKE_PROJECT_NAME} PRIVATE ${gettext_SOURCE_DIR}/include)
-    endif()
-  endif()
+  #     # set_target_properties(
+  #     #   gettext PROPERTIES IMPORTED_LOCATION ${CMAKE_SOURCE_DIR}/external/gettext/libgettextlib-0-21.dll
+  #     # )#.lib meybe or ${GETTEXT_LIBRARY}
+  #     # target_link_libraries(${CMAKE_PROJECT_NAME} PRIVATE ${DEPENDENCIES} gettext) # ${GETTEXT_LIBRARIES}
+  #     # target_include_directories(${CMAKE_PROJECT_NAME} PRIVATE ${gettext_SOURCE_DIR}/include)
+  #   endif()
+  # endif()
 
 endfunction()
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
 #include "TestPrint.hpp"
 
-#include <libintl.h>
-
 int main(int /*argc*/, const char ** /*argv*/) {
   TestPrint testPrint{};
   testPrint.init();
